d4de4be4e9e2c37a372e6eb0f8f2109ff8ae1011
Andrey141414 <andrusha.vinokurov@gmail.com>
2021-01-25 23:48:06 +0600

    Lab 7(1 branch)

---
+++ A/Chess_figure.cs
@@ -0,0 +1,81 @@
+﻿using System;
+namespace A
+{
+    public class Chess_figure
+    {
+        private String[] NAMES = { "Пешка", "Конь", "Слон", "Ладья", "Ферзь" };
+
+        private String name;
+        public String Name
+        {
+            //Можем получить значение вне класса 
+            get {
+                return name;
+            }
+            //Не можем присвоить значение вне класса 
+            private set {
+                name = value;
+            }
+        }
+
+        public int value;
+        public bool pawn;
+
+
+        public void Read()
+        {
+
+            int vibor;
+            Console.WriteLine("Название фигуры");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i + 1}  {NAMES[i]}");
+              
+            }
+
+
+            vibor = Convert.ToInt32(Console.ReadLine());
+
+            name = NAMES[vibor - 1];
+            switch (vibor)
+            {
+                case 1: { value = 1; pawn = true; } break;
+                case 2: { value = 3; } break;
+                case 3: { value = 3; } break;
+                case 4: { value = 5; } break;
+                case 5: { value = 9; } break;
+            }
+
+
+        }
+
+        //Использование this
+        public void Init(int value, String name)
+        {
+            this.value = value;
+            this.name = name;
+            if (this.name == NAMES[0])
+                pawn = true;
+            else
+                pawn = false;
+        }
+
+
+        public void Display()
+        {
+            Console.WriteLine($"{name} Ценность {value}");
+        }
+
+
+
+
+        public void getNameref(ref String name)
+        {
+            name = this.name;
+        }
+        public void getNameout(out String name)
+        {
+            name = this.name;
+        }
+    }
+}
---
+++ A/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace A
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+
+            String[] NAMES = { "Пешка", "Конь", "Слон", "Ладья", "Ферзь" };
+            Chess_figure[] MAS = new Chess_figure[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                MAS[i] = new Chess_figure();
+                switch (i % 5)
+                {
+                    case 0: { MAS[i].Init(1, NAMES[i % 5]); }; break;
+                    case 1: { MAS[i].Init(3, NAMES[i % 5]); }; break;
+                    case 2: { MAS[i].Init(3, NAMES[i % 5]); }; break;
+                    case 3: { MAS[i].Init(5, NAMES[i % 5]); }; break;
+                    case 4: { MAS[i].Init(9, NAMES[i % 5]); }; break;
+                }
+
+            }
+
+
+
+            int N = 10;
+            for (int i = 0; i < N; i++)
+            {
+                MAS[i].Display();
+            }
+
+
+
+            for (int i = 0; i < N; i++)
+            {
+
+                String a = MAS[i].Name;
+
+                for (int j = i + 1; j < N + 1; j++)
+                {
+                    String b = MAS[j].Name;
+                    if (String.Compare(a,b)==0)
+                    {
+                        for (int z = j; z < N - 1; z++)
+                        {
+                            MAS[z] = MAS[z + 1];
+                        }
+                        N -= 1;
+                    }
+                }
+            }
+
+            Console.WriteLine("\n\nМассив после обработки\n\n");
+            for (int i = 0; i < N; i++)
+            {
+                MAS[i].Display();
+            }
+
+
+
+
+
+            Chess_figure fig1 = new Chess_figure();
+            fig1.Init(3, "Slon");
+            Chess_figure fig2 = new Chess_figure();
+            fig2.Init(3, "Queen");
+
+
+            String str;
+            //Параметр не инициолизирован
+            fig1.getNameout(out str);
+            Console.WriteLine(str);
+
+            //Параметр инициолизирован
+            fig2.getNameref(ref str);
+            Console.WriteLine(str);
+
+
+
+
+
+
+            Chess_Player Magnus = new Chess_Player();
+            Magnus.playerInit("Magnus", 10, MAS, 1);
+
+
+        }
+    }
+}
